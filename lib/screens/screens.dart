41cabda1e5fd5ef4bdb0c0ae990e42e4e2d9b3d5
NadhirahZainal <nadhirah.zainal@swmsb.com>
2022-06-28 09:04:07 +0800

    removed duplicate import

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,10 +1,8 @@
-import 'package:eswm/screens/compactor_panel/compactor_panel.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 //import files
 import 'package:eswm/screens/pra/pra.dart';
-
 import '../utils/device.dart';
 import 'package:eswm/screens/compactor_panel/compactor_panel.dart';
 
@@ -16,7 +14,6 @@ class Screens extends StatefulWidget {
 }
 
 class _ScreensState extends State<Screens> {
-
   final Devices _device = Devices();
 
   @override
@@ -27,8 +24,6 @@ class _ScreensState extends State<Screens> {
 
   @override
   Widget build(BuildContext context) {
-    return _device.isPhone()
-        ? const Pra()
-        : const CompactorPanel();
+    return _device.isPhone() ? const Pra() : const CompactorPanel();
   }
 }
