88ebca5829555528186d474b68beb00f41791d03
NadhirahZainal <nadhirah.zainal@swmsb.com>
2022-07-19 17:08:08 +0800

    create status container for Laluan card

---
+++ lib/widgets/container/status_container.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class StatusContainer extends StatefulWidget {
+  Color boxColor;
+  String status;
+  Color textColor;
+  FontWeight fontWeight;
+
+  StatusContainer(
+      {Key? key,
+      required this.boxColor,
+      required this.status,
+      required this.textColor,
+      required this.fontWeight})
+      : super(key: key);
+
+  @override
+  State<StatusContainer> createState() => _StatusContainerState();
+}
+
+class _StatusContainerState extends State<StatusContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 7),
+      decoration: BoxDecoration(
+        color: widget.boxColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(
+        widget.status,
+        style: TextStyle(
+          color: widget.textColor,
+          fontWeight: widget.fontWeight,
+        ),
+      ),
+    );
+  }
+}
