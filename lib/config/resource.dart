075b1dad77c24856a9a0b2de6ddd141fc4623b97
Sahanah A/P Pandu Rengan <spr001@lgb.group>
2022-08-04 10:22:20 +0800

    [iWork Dev] Improve drawer styling for all users

--- lib/config/resource.dart
+++ lib/config/resource.dart
@@ -6,3 +6,4 @@ String operationImg = 'assets/image/operation.png';
 String logoImg = 'assets/image/logo.png';
 String headerImg = 'assets/image/header.png';
 String idleImg = 'assets/image/idle.png';
+String userImg = 'assets/image/user.jpg';
