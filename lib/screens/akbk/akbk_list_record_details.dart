056ab6ed514b617296153f8b33d7aed873fb89ae
NadhirahZainal <nadhirah.zainal@swmsb.com>
2022-08-12 17:09:30 +0800

    Removed unneccesary container

--- lib/screens/akbk/akbk_list_record_details.dart
+++ lib/screens/akbk/akbk_list_record_details.dart
@@ -17,86 +17,82 @@ class AkbkListRecordDetails extends StatefulWidget {
 class _AkbkListRecordDetailsState extends State<AkbkListRecordDetails> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  widget.data.vehicleNo,
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.data.vehicleNo,
+                style: TextStyle(
+                    fontWeight: FontWeight.w700, color: black87, fontSize: 15),
+              ),
+              StatusContainer(
+                type: "Akbk",
+                status: widget.data.status,
+                statusId: widget.data.statusId,
+                fontWeight: statusFontWeight,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('No. AKBK',
                   style: TextStyle(
                       fontWeight: FontWeight.w700,
                       color: black87,
-                      fontSize: 15),
-                ),
-                StatusContainer(
-                  type: "Akbk",
-                  status: widget.data.status,
-                  statusId: widget.data.statusId,
-                  fontWeight: statusFontWeight,
-                ),
-              ],
-            ),
+                      fontSize: 13)),
+              Text(widget.data.akbkNo,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: grey500,
+                      fontSize: 13)),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10, 10, 10, 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('No. AKBK',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        color: black87,
-                        fontSize: 13)),
-                Text(widget.data.akbkNo,
-                    style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        color: grey500,
-                        fontSize: 13)),
-              ],
-            ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Jenis Kerosakan',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: black87,
+                      fontSize: 13)),
+              Text(widget.data.breakdownType,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: grey500,
+                      fontSize: 13)),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Jenis Kerosakan',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        color: black87,
-                        fontSize: 13)),
-                Text(widget.data.breakdownType,
-                    style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        color: grey500,
-                        fontSize: 13)),
-              ],
-            ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Tarikh',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: black87,
+                      fontSize: 13)),
+              Text(widget.data.date,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: grey500,
+                      fontSize: 13)),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Tarikh',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        color: black87,
-                        fontSize: 13)),
-                Text(widget.data.date,
-                    style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        color: grey500,
-                        fontSize: 13)),
-              ],
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
