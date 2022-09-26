afd6c4f723fc862f040294a8279f7ec7774dc178
NadhirahZainal <nadhirah.zainal@swmsb.com>
2022-09-26 08:50:37 +0800

    Comment unused parameter

--- lib/widgets/cards/my_task/supervisor/supervisor_my_task_list_details_v1.dart
+++ lib/widgets/cards/my_task/supervisor/supervisor_my_task_list_details_v1.dart
@@ -221,7 +221,7 @@ class _SupervisorMyTaskListDetailsState
                             type: PageTransitionType.fade,
                             child: ScheduleIssueMainScreen(
                                 laluanData: widget.data,
-                                fromHome: true,
+                                //fromHome: true,
                                 issueType: widget.data.isu)));
                   },
                 ),
