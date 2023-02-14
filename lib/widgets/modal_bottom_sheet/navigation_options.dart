392b9babf31e999c96f8484124214e67eb35277d
Sahanah Pandu Rengan <sahanah.pandurengan@swmsb.com>
2023-02-14 13:58:09 +0800

    [iWork Dev] Add navigation function for street search

--- lib/widgets/modal_bottom_sheet/navigation_options.dart
+++ lib/widgets/modal_bottom_sheet/navigation_options.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 //import files
 import '../../config/palette.dart';
@@ -56,28 +57,38 @@ Widget? showNavigationOptions(BuildContext context) {
                   top: 10,
                   right: 24,
                 ),
-                child: Text(
-                  "Waze",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: blackCustom,
-                    fontWeight: FontWeight.w400,
+                child: GestureDetector(
+                  onTap: () {
+                    _launchWaze(3.1392412891357626, 101.62878692150117);
+                  },
+                  child: Text(
+                    "Waze",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: blackCustom,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
               ),
               const SizedBox(
                 height: 24,
               ),
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 24,
-                ),
-                child: Text(
-                  "Google Maps",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: blackCustom,
-                    fontWeight: FontWeight.w400,
+              GestureDetector(
+                onTap: () {
+                  _launchMaps(3.1392412891357626, 101.62878692150117);
+                },
+                child: Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                  ),
+                  child: Text(
+                    "Google Maps",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: blackCustom,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
               ),
@@ -86,4 +97,32 @@ Widget? showNavigationOptions(BuildContext context) {
         );
       });
   return null;
+}
+
+void _launchMaps(double lat, double lng) async {
+  dynamic url =
+      'http://maps.google.com/maps?q=${lat.toString()},${lng.toString()}}';
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+void _launchWaze(double lat, double lng) async {
+  var url = 'waze://?ll=${lat.toString()},${lng.toString()}';
+  var fallbackUrl =
+      'https://waze.com/ul?ll=${lat.toString()},${lng.toString()}&navigate=yes';
+  try {
+    bool launched =
+        // ignore: deprecated_member_use
+        await launch(url, forceSafariVC: false, forceWebView: false);
+    if (!launched) {
+      // ignore: deprecated_member_use
+      await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+    }
+  } catch (e) {
+    // ignore: deprecated_member_use
+    await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+  }
 }
