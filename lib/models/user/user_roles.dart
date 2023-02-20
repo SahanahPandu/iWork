9d0f868f96678480efc2d7bed30b9b299a432398
NadhirahZainal <nadhirah.zainal@swmsb.com>
2023-02-20 17:30:52 +0800

    create model for user role

---
+++ lib/models/user/user_roles.dart
@@ -0,0 +1,27 @@
+class UserRole {
+  UserRole({
+    required this.id,
+    required this.userId,
+    required this.roleCode,
+    required this.roleDesc,
+  });
+
+  int id;
+  int userId;
+  String roleCode;
+  String roleDesc;
+
+  factory UserRole.fromJson(Map<String, dynamic> json) => UserRole(
+        id: json["id"],
+        userId: json["user_id"],
+        roleCode: json["role_code"],
+        roleDesc: json["role_desc"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "user_id": userId,
+        "role_code": roleCode,
+        "role_desc": roleDesc,
+      };
+}
